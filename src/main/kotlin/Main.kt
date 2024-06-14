73271d8a7f350e90851382dc30a66cfed3137368
Nikita-Vesel0v <nikiveselo2004@gmail.com>
2024-06-14 21:43:25 +0300

    add encryption with key that swipe right alphabet symbols in cipher

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,15 @@
 package encryptdecrypt
 
+const val ALPHABET = "abcdefghijklmnopqrstuvwxyz"
+
 fun main() {
-    val codeDict = buildMap { for (ch in 'a'..'z') this[ch] = 'z' - (ch - 'a') }
-
-    val toEncrypt = "we found a treasure!"
-
+    val toEncrypt = readln()
+    val key = readln().toInt()
+    val codeDict = buildMap {
+        for (ch in 0..25) {
+            this[ALPHABET[ch]] = ALPHABET[(ch + key) % 26]
+        }
+    }
     val codedInput = buildString {
         toEncrypt.lowercase().forEach {
             append(if (it in codeDict) codeDict[it] else it)
