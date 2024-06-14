9328e36508ca478633da0a1b091d8ae4f5a6199d
Nikita-Vesel0v <nikiveselo2004@gmail.com>
2024-06-14 22:46:05 +0300

    add reading input data from Program arguments

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,26 +1,39 @@
 package encryptdecrypt
 
-class Cryptographer {
-    private lateinit var action: String // encrypt or decrypt
-    private lateinit var input: String
+class Cryptographer(args: List<String>) {
+    private lateinit var mode: String // encrypt or decrypt
+    private lateinit var data: String
     private lateinit var output: String
     private var key = 0
 
+    init {
+        for (i in 0..2) {
+            when (args[i * 2]) {
+                "-mode" -> mode = args[i * 2 + 1]
+                "-data" -> data = args[i * 2 + 1]
+                "-key" -> key = args[i * 2 + 1].toInt()
+                else -> throw IllegalArgumentException("Invalid input data: ${args.joinToString(" ")}")
+            }
+        }
+    }
+
     fun menu() {
-        action = readln()
-        input = readln()
-        key = readln().toInt()
-        output = when (action.lowercase()) {
-            "enc" -> input.map { it + key }.joinToString("")
-            "dec" -> input.map { it - key }.joinToString("")
-            else -> { println("Unknown action: $action"); return }
+        output = when (mode.lowercase()) {
+            "enc" -> data.map { it + key }.joinToString("")
+            "dec" -> data.map { it - key }.joinToString("")
+            else -> { println("Unknown action: $mode"); return }
         }
         println(output)
     }
-
-
 }
-fun main() {
-    val cryptographer = Cryptographer()
+fun createArgs(): List<String> = buildList {
+    print("Mode: "); add("-mode"); add(readln())
+    print("Data: "); add("-data"); add(readln())
+    print("Key: "); add("-key"); add(readln())
+}
+
+fun main(args: Array<String>) {
+    val newArgs = if (args.size == 6) args.toList() else createArgs()
+    val cryptographer = Cryptographer(newArgs)
     cryptographer.menu()
 }
