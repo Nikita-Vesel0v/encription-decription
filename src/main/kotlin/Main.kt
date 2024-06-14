c6efa9690282ad6a920beace5d3414eabd992c87
Nikita-Vesel0v <nikiveselo2004@gmail.com>
2024-06-15 01:28:30 +0300

    add reading from file, writing result to file and options by default for program args

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,39 +1,47 @@
 package encryptdecrypt
 
-class Cryptographer(args: List<String>) {
-    private lateinit var mode: String // encrypt or decrypt
-    private lateinit var data: String
-    private lateinit var output: String
-    private var key = 0
+import java.io.File
+
+class Cryptographer(args: Array<String>) {
+    private var mode: String = "enc"
+    private var data: String = ""
+    private var key: Int = 0
+    private var output = ""
+    private var outputFilename = ""
 
     init {
-        for (i in 0..2) {
+        for (i in 0 until args.size  / 2) {
             when (args[i * 2]) {
                 "-mode" -> mode = args[i * 2 + 1]
                 "-data" -> data = args[i * 2 + 1]
                 "-key" -> key = args[i * 2 + 1].toInt()
-                else -> throw IllegalArgumentException("Invalid input data: ${args.joinToString(" ")}")
+                "-in" -> data = File(args[i * 2 + 1]).readText()
+                "-out" -> outputFilename = args[i * 2 + 1]
+                else -> throw IllegalArgumentException("Error Invalid input data: ${args.joinToString(" ")}")
             }
         }
     }
-
     fun menu() {
         output = when (mode.lowercase()) {
             "enc" -> data.map { it + key }.joinToString("")
             "dec" -> data.map { it - key }.joinToString("")
             else -> { println("Unknown action: $mode"); return }
         }
-        println(output)
+
+        if (outputFilename.isNotEmpty()) File(outputFilename).writeText(output)
+        else println(output)
     }
 }
-fun createArgs(): List<String> = buildList {
-    print("Mode: "); add("-mode"); add(readln())
-    print("Data: "); add("-data"); add(readln())
-    print("Key: "); add("-key"); add(readln())
+fun createArgs(): Array<String> {
+    var args = arrayOf<String>()
+    print("Mode: "); args += "-mode"; args += readln()
+    print("Data: "); args += "-data"; args += readln()
+    print("Key: "); args += "-key"; args += readln()
+    return args
 }
 
 fun main(args: Array<String>) {
-    val newArgs = if (args.size == 6) args.toList() else createArgs()
-    val cryptographer = Cryptographer(newArgs)
+    val programArgs = if (args.isEmpty()) createArgs() else args
+    val cryptographer = Cryptographer(programArgs)
     cryptographer.menu()
 }
