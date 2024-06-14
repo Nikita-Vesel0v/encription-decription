92828896b3869aae0109f0d89c01bcbae755cd9d
Nikita-Vesel0v <nikiveselo2004@gmail.com>
2024-06-14 22:17:20 +0300

    add function decryption in the menu

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,46 +1,23 @@
 package encryptdecrypt
 
-const val ALPHABET = "abcdefghijklmnopqrstuvwxyz"
-
 class Cryptographer {
-    private var input = ""
-    private var output = ""
+    private lateinit var action: String // encrypt or decrypt
+    private lateinit var input: String
+    private lateinit var output: String
     private var key = 0
-    private var codeDict = emptyMap<Char, Char>()
 
     fun menu() {
-        val action = readln()
-        when (action.lowercase()) {
-            "enc" -> encryption()
-            "dec" -> TODO()
+        action = readln()
+        input = readln()
+        key = readln().toInt()
+        output = when (action.lowercase()) {
+            "enc" -> input.map { it + key }.joinToString("")
+            "dec" -> input.map { it - key }.joinToString("")
             else -> { println("Unknown action: $action"); return }
         }
         println(output)
-
     }
 
-    private fun createCodeDict() {
-        codeDict = buildMap {
-            for (ch in 0..25) {
-                this[ALPHABET[ch]] = ALPHABET[(ch + key) % 26]
-            }
-        }
-    }
-
-    private fun encryption() {
-        input = readln()
-        key = readln().toInt()
-        createCodeDict()
-        encrypt()
-    }
-
-    private fun encrypt() {
-        output = buildString {
-            input.lowercase().forEach {
-                append(if (it in codeDict) codeDict[it] else it)
-            }
-        }
-    }
 
 }
 fun main() {
